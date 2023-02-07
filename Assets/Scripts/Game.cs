50afdefd93a4a24823d13e5ee0bb814988d81fb7
Eero12 <eerooja18@gmail.com>
2023-02-07 09:39:12 +0200

    crit

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -9,26 +9,58 @@ public class Game : MonoBehaviour
 {
     public TextMeshProUGUI MoneyCounter;
     public TextMeshProUGUI Upgrade1Counter;
+    public TextMeshProUGUI Upgrade2Counter;
 
     public void Increment()
     {
-        GameManager.money += GameManager.multiplier;
+        GameManager.randomnumber2 = Random.Range(GameManager.Range0, GameManager.range100);
+        GameManager.randomnumber1 = Random.Range(GameManager.Range0, GameManager.Range100);
+        if (GameManager.randomnumber1 == GameManager.randomnumber2)
+        {
+            GameManager.money += GameManager.multiplier * GameManager.CritPercent;
+            Debug.Log("CRITICAL!!");
+        }
+        else
+        {
+            GameManager.money += GameManager.multiplier;
+        }
     }
-
-    public void Buy()
-    {
-        Debug.Log(GameManager.Upgrade1);
+    public void Values()
+    {        
+        Debug.Log("RandomNumber1: " + GameManager.randomnumber1);
+        Debug.Log("RandomNumber2: " + GameManager.randomnumber2);
+        Debug.Log("Range is: " + GameManager.range100);
+        
+    }
+    public void BuyUpgrade1()
+    {        
         if(GameManager.money >= GameManager.Upgrade1) 
         {
             GameManager.multiplier += 1;
             GameManager.money -= GameManager.Upgrade1;
             GameManager.Upgrade1 += 50;
         }
+    }  public void BuyUpgrade2()
+    {        
+
+        if(GameManager.money >= GameManager.Upgrade2) 
+        {
+            GameManager.Range100 -= 10;
+            GameManager.range100 -= 10;
+            GameManager.money -= GameManager.Upgrade2;
+            //GameManager.Upgrade2 += 50;
+            if (GameManager.range100 <= 0)
+            {
+                GameManager.range100 = 0;
+                GameManager.Range100 = 0;
+            }
+        }
     }
 
     public void Update()
     {
-        MoneyCounter.text = "Price: " + GameManager.Upgrade1;
-        Upgrade1Counter.text = "Money: " + GameManager.money;
+        MoneyCounter.text = "Money: " + GameManager.money;
+        Upgrade1Counter.text = ""+GameManager.Upgrade1;
+        Upgrade2Counter.text = ""+GameManager.Upgrade2;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static float Upgrade1;
+    public static float Upgrade2;
     public static float money;
     public static float multiplier;
+    public static float CritPercent;
+    public static int randomnumber1;
+    public static int randomnumber2;
+    public static int Range0;
+    public static int Range100;
+    public static int range100;
     void Start()
-    {
+    { 
         Upgrade1 = 25;
+        Upgrade2 = 25;
         multiplier = 1;
         money = 0;
+        CritPercent = 100;
+        Range0 = 0;
+        Range100 = 100;
+        range100 = 100;
     }
 
 
